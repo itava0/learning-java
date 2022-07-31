db3737e338ec733a9307fe21d0266e934f6e0bce
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-31 02:40:03 -0400

    if statement

--- src/com/itava0/Main.java
+++ src/com/itava0/Main.java
@@ -2,6 +2,11 @@ package com.itava0;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int johnAge = 18;
+        if ( johnAge <= 21) {
+            System.out.println("Not old enough to drink");
+        } else {
+            System.out.println("Hooray...old enough to drink");
+        }
     }
 }
