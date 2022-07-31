7d0fcee3e4a57e81647076be97580f4729918198
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-31 03:07:38 -0400

    Array and loops

--- src/com/itava0/Main.java
+++ src/com/itava0/Main.java
@@ -2,11 +2,11 @@ package com.itava0;
 
 public class Main {
     public static void main(String[] args) {
-        int johnAge = 18;
-        if ( johnAge <= 21) {
-            System.out.println("Not old enough to drink");
-        } else {
-            System.out.println("Hooray...old enough to drink");
+        // Arrays
+        int [] numbers = {2,5,6,9};
+
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
