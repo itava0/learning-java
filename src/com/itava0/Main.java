36e102b2b64bdcce0b364b0ae7470536f0349230
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-08-01 07:36:38 -0400

    Scanner class

--- src/com/itava0/Main.java
+++ src/com/itava0/Main.java
@@ -1,15 +1,26 @@
 package com.itava0;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Arrays
-        String[] names = {"Anna", "Ali", "Bob", "Mike"};
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
 
-        for (String name : names) {
-            if (name.equals("Mike")) {
-                break;
-            }
-            System.out.println(name);
+        String userName = scanner.nextLine();
+        System.out.println("Hello " + userName);
+
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(scanner.nextLine());
+        int year = LocalDate.now().minusYears(age).getYear();
+        System.out.println("Your were born in " + year);
+
+        if(age < 18) {
+            System.out.println("and you are not an adult");
+        } else {
+            System.out.println("and you are an adult");
         }
     }
 }
