c30d64d866d1008910b471d30a8e8a1037d0c235
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-08-07 05:42:12 -0400

    Classes and Objects

--- src/com/itava0/Main.java
+++ src/com/itava0/Main.java
@@ -1,11 +1,21 @@
 package com.itava0;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         // Classes and Objects
         Lens lensOne = new Lens("sony", "85mm", true);
 
         System.out.println(lensOne.brand);
+
+        Passport usPassport = new Passport("1254", LocalDate.of(2025, 1, 16));
+        System.out.println("US Passport");
+        System.out.println(usPassport.number);
+        System.out.println(usPassport.expireDate);
     }
+
+
 
     static class Lens {
         String brand;
