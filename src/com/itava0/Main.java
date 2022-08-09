0f7620f0cf185b6ebbae2396871040123f3213f8
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-08-09 08:32:55 -0400

    Arraylist and Map

--- src/com/itava0/Main.java
+++ src/com/itava0/Main.java
@@ -1,28 +1,36 @@
 package com.itava0;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         // Sets and Lists
-        Set<Integer> t = new HashSet<Integer>();
+        Set<Integer> t = new HashSet<>();
         //Collection of unordered and unique items
         t.add(5);
         t.add(7);
         t.add(6);
 
-       boolean x = t.contains(5);
+        System.out.println("Sets " + t);
 
-        ArrayList<Integer> num = new ArrayList<Integer>();
+        ArrayList<Integer> num = new ArrayList<>();
         num.add(1);
         num.add(2);
-        num.get(0);
         num.set(1, 5);
 
-        System.out.println(num);
+        System.out.println("ArrayList " + num);
+
+
+        //Maps and HashMaps
+
+        //Similar to Dictionary in python, it stores data values in key:value pairs.
+        Map m = new HashMap();
+        m.put("tim", 5);
+        m.put("joe", 6);
+        m.put("bob", 7);
+
+
+        System.out.println("Map " + m);
     }
 
 }
